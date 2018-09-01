e7a2f8dc7756c58417e8be51e2bcdef1f92c071c
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-09-01 20:06:19 -0300

    Physics Version 1.0
    - Implemented Physics to Lemmings and Removed Character Controller
    - Implemented Lane Movement
    - Implemented Object Pool to Lemming Spawner

---
+++ Assets/Scripts/Physics/LemmingMovementController.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LemmingStateController))]
+public class LemmingMovementController : MonoBehaviour
+{
+    //Internal Variables
+    private float movementSpeed = 3;
+    private LayerMask wallsLayerMask;
+    private RaycastHit[] raycastHits;
+
+    //Reference Variables
+    private LemmingStateController lemmingStateController;
+    [SerializeField]
+    private Vector3 nextWaypoint;
+    private Vector3 movementDirection;
+
+    //Awake Method
+    private void Awake()
+    {
+        lemmingStateController = this.GetComponent<LemmingStateController>();
+    }
+
+    //Start Method
+    void Start ()
+    {
+        nextWaypoint = this.transform.position;
+        movementDirection = Vector3.right;
+        wallsLayerMask = LayerMask.GetMask("Wall");
+        raycastHits = new RaycastHit[1];
+    }
+
+    //Set Direction
+    public void setDirection(Vector3 newDirection)
+    {
+        movementDirection = newDirection;
+    }
+
+    //Fixed Update Method
+    private void FixedUpdate()
+    {
+        if ((nextWaypoint - this.transform.position) == Vector3.zero)
+        {
+            //Check Floor
+            int hits = Physics.RaycastNonAlloc(this.transform.position, Vector3.down, raycastHits, 1f, wallsLayerMask);
+            if (hits == 0)
+            {
+                nextWaypoint.Set(this.transform.position.x, this.transform.position.y - 1f, this.transform.position.z);
+                //TODO (Remove Skills, etc)
+            }
+            else
+            {
+                //Check Walls
+                hits = Physics.RaycastNonAlloc(this.transform.position, movementDirection, raycastHits, 0.45f, wallsLayerMask);
+                if (hits == 1)
+                {
+                    if (movementDirection == Vector3.right)
+                    {
+                        setDirection(Vector3.left);
+                        nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.6f);
+                    }
+                    else if (movementDirection == Vector3.left)
+                    {
+                        setDirection(Vector3.right);
+                        nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z + 0.6f);
+                    }
+                    else if (movementDirection == Vector3.forward)
+                    {
+                        setDirection(Vector3.back);
+                        nextWaypoint.Set(this.transform.position.x - 0.6f, this.transform.position.y, this.transform.position.z);
+                    }
+                    else if (movementDirection == Vector3.back)
+                    {
+                        setDirection(Vector3.forward);
+                        nextWaypoint.Set(this.transform.position.x + 0.6f, this.transform.position.y, this.transform.position.z);
+                    }
+                }
+                else updateWaypoint();
+            }
+        }
+        //Move Lemming
+        else this.transform.position = Vector3.MoveTowards(this.transform.position, nextWaypoint, movementSpeed * Time.deltaTime);
+    }
+
+    //On Trigger Enter
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Box Collider Enter");
+        //TODO: Lemming Actions
+    }
+
+    //Main Movement Logic
+    private void updateWaypoint()
+    {
+        if (movementDirection == Vector3.right)
+        {
+            if (this.transform.position.x < 0f)
+            {
+                if(System.Math.Round(Mathf.Abs(this.transform.position.x % 1), 2) == 0.3) nextWaypoint.Set(this.transform.position.x + 0.6f, this.transform.position.y, this.transform.position.z);
+                else nextWaypoint.Set(this.transform.position.x + 0.4f, this.transform.position.y, this.transform.position.z);
+            }
+            else
+            {
+                if (System.Math.Round(Mathf.Abs(this.transform.position.x % 1), 2) == 0.7) nextWaypoint.Set(this.transform.position.x + 0.6f, this.transform.position.y, this.transform.position.z);
+                else nextWaypoint.Set(this.transform.position.x + 0.4f, this.transform.position.y, this.transform.position.z);
+            }
+        }
+        else if (movementDirection == Vector3.left)
+        {
+            if (this.transform.position.x < 0f)
+            {
+                if (System.Math.Round(Mathf.Abs(this.transform.position.x % 1), 2) == 0.3) nextWaypoint.Set(this.transform.position.x - 0.4f, this.transform.position.y, this.transform.position.z);
+                else nextWaypoint.Set(this.transform.position.x - 0.6f, this.transform.position.y, this.transform.position.z);
+            }
+            else
+            {
+                if (System.Math.Round(Mathf.Abs(this.transform.position.x % 1), 2) == 0.7) nextWaypoint.Set(this.transform.position.x - 0.4f, this.transform.position.y, this.transform.position.z);
+                else nextWaypoint.Set(this.transform.position.x - 0.6f, this.transform.position.y, this.transform.position.z);
+            }
+        }
+        else if (movementDirection == Vector3.forward)
+        {
+            if (this.transform.position.z < 0f)
+            {
+                if (System.Math.Round(Mathf.Abs(this.transform.position.z % 1), 2) == 0.3) nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z + 0.4f);
+                else nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z + 0.6f);
+            }
+            else
+            {
+                if (System.Math.Round(Mathf.Abs(this.transform.position.z % 1), 2) == 0.7) nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z + 0.4f);
+                else nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z + 0.6f);
+            }
+        }
+        else if (movementDirection == Vector3.back)
+        {
+            if (this.transform.position.z < 0f)
+            {
+                if (System.Math.Round(Mathf.Abs(this.transform.position.z % 1), 2) == 0.3) nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.6f);
+                else nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.4f);
+            }
+            else
+            {
+                if (System.Math.Round(Mathf.Abs(this.transform.position.z % 1), 2) == 0.7) nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.6f);
+                else nextWaypoint.Set(this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.4f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Physics/LemmingStateController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Skill
+{
+    None,
+    Basher,             //Horizontal Dig
+    Blocker_TurnNorth,  //Change Direction (North)
+    Blocker_TurnEast,   //Change Direction (East)
+    Blocker_TurnSouth,   //Change Direction (South)
+    Blocker_TurnWest,   //Change Direction (West)
+    Builder,            //Stairs
+    Climber,            //Climb Walls
+    Digger,             //Vertical Dig
+    Exploder,           //Self-Destruct
+    Floater             //Umbrella
+    //Miner             //Diagonal Dig
+}
+
+public class LemmingStateController : MonoBehaviour
+{
+    //Variables
+    private bool[] skillsArray;
+
+    //Start Method
+    private void Start()
+    {
+        //Initialize Skills Array
+        skillsArray = new bool[10];
+        for(int i = 0; i < 10; i++)
+        {
+            skillsArray[i] = false;
+        }
+    }
+
+    //Check Lemming for a certain skill
+    public bool checkSkill(Skill skill)
+    {
+        switch(skill)
+        {
+            case Skill.Basher:
+                return skillsArray[0];
+            case Skill.Blocker_TurnNorth:
+                return skillsArray[1];
+            case Skill.Blocker_TurnEast:
+                return skillsArray[2];
+            case Skill.Blocker_TurnSouth:
+                return skillsArray[3];
+            case Skill.Blocker_TurnWest:
+                return skillsArray[4];
+            case Skill.Builder:
+                return skillsArray[5];
+            case Skill.Climber:
+                return skillsArray[6];
+            case Skill.Digger:
+                return skillsArray[7];
+            case Skill.Exploder:
+                return skillsArray[8];
+            case Skill.Floater:
+                return skillsArray[9];
+            default: return false;
+        }
+    }
+
+    //Set a certain skill for Lemming
+    public void setSkill(Skill skill, bool value)
+    {
+        switch (skill)
+        {
+            case Skill.Basher:
+                skillsArray[0] = value;
+                break;
+            case Skill.Blocker_TurnNorth:
+                skillsArray[1] = value;
+                break;
+            case Skill.Blocker_TurnEast:
+                skillsArray[2] = value;
+                break;
+            case Skill.Blocker_TurnSouth:
+                skillsArray[3] = value;
+                break;
+            case Skill.Blocker_TurnWest:
+                skillsArray[4] = value;
+                break;
+            case Skill.Builder:
+                skillsArray[5] = value;
+                break;
+            case Skill.Climber:
+                skillsArray[6] = value;
+                break;
+            case Skill.Digger:
+                skillsArray[7] = value;
+                break;
+            case Skill.Exploder:
+                skillsArray[8] = value;
+                break;
+            case Skill.Floater:
+                skillsArray[9] = value;
+                break;
+        }
+    }
+}
