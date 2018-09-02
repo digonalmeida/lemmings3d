1446c131b96e44c7a7ee6e427890a1f52e0c0f5f
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2018-09-02 01:15:19 -0300

    Game HUD Concept

---
+++ Assets/Scripts/Dock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Dock : MonoBehaviour {
+
+    public UnityEvent pause;
+    public UnityEvent explodeAll;
+    public UnityEvent decreaseRate;
+    public UnityEvent increaseRate;
+	public UnityEvent turnSkill;
+    public UnityEvent parachuteSkill;
+
+    public void PauseButton()
+    {
+        pause.Invoke();
+    }
+
+    public void ExplodeAllButton()
+    {
+        explodeAll.Invoke();
+    }
+
+    public void DecreaseRateButton()
+    {
+        decreaseRate.Invoke();
+    }
+
+    public void IncreaseRateButton()
+    {
+        increaseRate.Invoke();
+    }
+
+    public void TurnSkillButton()
+    {
+        turnSkill.Invoke();
+    }
+
+    public void ParachuteSkillButton()
+    {
+        parachuteSkill.Invoke();
+    }
+
+}
