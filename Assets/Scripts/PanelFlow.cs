d2e6d87b3c9902789c2d4d36e163a48fd4f1de4d
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-10-10 16:49:14 -0300

    Finished Menu

--- Assets/Scripts/PanelFlow.cs
+++ Assets/Scripts/PanelFlow.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PanelFlow : MonoBehaviour
 {
     //Variables
     public GameObject[] panels;
+    private int activePanel;
+
+    //Start
+    private void Start()
+    {
+        activePanel = 0;
+    }
 
     //Change Panel Method
-    public void changePanel(int currentPanel, int nextPanel)
+    public void changePanel(int nextPanel)
     {
-        panels[currentPanel].SetActive(false);
+        panels[activePanel].SetActive(false);
         panels[nextPanel].SetActive(true);
+        activePanel = nextPanel;
+    }
+
+    //Quit Game
+    public void quitGame()
+    {
+        Application.Quit();
     }
 }
