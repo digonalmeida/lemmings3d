85a066cdc499dcb564cbdfa8c32d8c89fd50968e
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2018-10-11 23:37:20 -0300

    Arrow Button and shader

---
+++ Assets/BlockerDirectionSelector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockerDirectionSelector : MonoBehaviour {
+
+    private Transform followingTransform;
+
+
+    public void Setup(Transform following)
+    {
+        followingTransform = following;
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = followingTransform.position;
+
+	}
+
+    public void ClickDirection(int direction)
+    {
+        switch(direction)
+        {
+            
+        }
+
+        Hide();
+
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
