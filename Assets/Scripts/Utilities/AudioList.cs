1291f6acc979fc5ff2c2ec7a6a4945db8615bd7d
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2018-10-27 17:20:59 -0300

    random audio scriptable object

---
+++ Assets/Scripts/Utilities/AudioList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioList", menuName ="", order =0)]
+public class AudioList : ScriptableObject {
+
+    [SerializeField] private List<AudioClip> audios = new List<AudioClip>();
+    private AudioClip[] uniqueAudios = new AudioClip[0];
+
+    public AudioClip GetRandom() {
+        if (audios.Count <= 0) return null;
+        return audios[Random.Range(0, audios.Count)];
+    }
+
+    public AudioClip GetUniqueRandom() {
+        if (audios.Count <= 0) return null;
+        if (uniqueAudios.Length <= 0) uniqueAudios = audios.ToArray();
+        uniqueAudios.Pop
+    }
+
+}
