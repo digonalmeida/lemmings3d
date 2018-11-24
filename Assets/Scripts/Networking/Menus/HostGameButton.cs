dd8c4f83ccd39a0e7b6d0aafa4a8c3770d172440
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-11-23 22:25:52 -0200

    Progress on Menu

---
+++ Assets/Scripts/Networking/Menus/HostGameButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+public class HostGameButton : MonoBehaviour
+{
+    //References
+    private string roomName;
+
+    //Update Room Name
+    public void updateRoomName(string name)
+    {
+        roomName = name;
+    }
+
+    //Create Lan Match
+    public void CreateMatch()
+    {
+        if (!string.IsNullOrEmpty(roomName))
+        {
+            LNetworkLobbyManager networkManager = LNetworkLobbyManager.singleton.GetComponent<LNetworkLobbyManager>();
+            networkManager.matchMaker.CreateMatch(roomName, 2, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
