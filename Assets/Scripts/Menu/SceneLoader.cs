3565c57c6140c3a626bde6540ac0e39abe98e60f
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-11-23 22:10:15 -0200

    Network Manager Moved to Initial Test Scene

---
+++ Assets/Scripts/Menu/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneName
+{
+    Menu,
+    Lobby,
+    Level
+}
+
+public class SceneLoader : MonoBehaviour 
+{
+	//Reference
+	public SceneName sceneToLoad;
+
+	// Use this for initialization
+	void Start ()
+	{
+        if (sceneToLoad == SceneName.Menu)
+        {
+            SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+        }
+        else if (sceneToLoad == SceneName.Level)
+        {
+            SceneManager.LoadScene("DefaultLevel", LoadSceneMode.Single);
+        }
+    }
+}
