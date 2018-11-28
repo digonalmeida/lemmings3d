bb3dd03d7b65ca1ced5f73d90bf840635c11c14d
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-11-28 20:04:52 -0200

    Major Updates
    - Remade All Menus
    - Created Lobby
    - Finished Lemming Customization in Lobby
    - Finished Customization Sync between Clients

--- Assets/Scripts/Menu/LobbyPanelManager.cs
+++ Assets/Scripts/Menu/LobbyPanelManager.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class LobbyPanelManager : MonoBehaviour
+public class LobbyPanelManager : NetworkBehaviour
 {
+    //Variables
+    private bool readyToBegin = false;
+
+    //Network
+    private LNetworkLobbyPlayer player;
+    private LNetworkLobbyPlayer opponent;
+
     //References
     public GameObject player1Lemming;
     public GameObject player2Lemming;
     public Text player1Text;
     public Text player2Text;
+    public GameObject previousColorPanelPlayer1;
+    public GameObject previousColorPanelPlayer2;
+    public GameObject nextColorPanelPlayer1;
+    public GameObject nextColorPanelPlayer2;
+    public Sprite readySprite;
+    public Sprite unreadySprite;
+    public Image readyButtonCheckbox;
 
     //Singleton
     private static LobbyPanelManager instance;
@@ -27,7 +42,7 @@ public class LobbyPanelManager : MonoBehaviour
         //Check Singleton
         if (instance != null && instance != this)
         {
-            Destroy(this);
+            Destroy(this.gameObject);
         }
         else
         {
@@ -35,9 +50,76 @@ public class LobbyPanelManager : MonoBehaviour
         }
     }
 
-    //On Object Destroy (Safeguard)
-    public void OnDestroy()
+    //Set Player
+    public void setPlayerButtons(Player playerNum)
     {
-        instance = null;
+        if(playerNum == Player.Player1)
+        {
+            for (int i = 0; i < previousColorPanelPlayer2.transform.childCount; i++)
+            {
+                previousColorPanelPlayer2.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            for (int i = 0; i < nextColorPanelPlayer2.transform.childCount; i++)
+            {
+                nextColorPanelPlayer2.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < previousColorPanelPlayer1.transform.childCount; i++)
+            {
+                previousColorPanelPlayer1.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            for (int i = 0; i < nextColorPanelPlayer1.transform.childCount; i++)
+            {
+                nextColorPanelPlayer1.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    //Request Previous Hair
+    public void requestPreviousHair()
+    {
+        if (player == null) player = LNetworkLobbyPlayer.getLocalLobbyPlayer();
+        player.CmdRequestPreviousHairColor();
+    }
+
+    //Request Next Hair
+    public void requestNextHair()
+    {
+        if (player == null) player = LNetworkLobbyPlayer.getLocalLobbyPlayer();
+        player.CmdRequestNextHairColor();
+    }
+
+    //Request Previous Cloth
+    public void requestPreviousCloth()
+    {
+        if (player == null) player = LNetworkLobbyPlayer.getLocalLobbyPlayer();
+        player.CmdRequestPreviousClothColor();
+    }
+
+    //Request Next Cloth
+    public void requestNextCloth()
+    {
+        if (player == null) player = LNetworkLobbyPlayer.getLocalLobbyPlayer();
+        player.CmdRequestNextClothColor();
+    }
+
+    //Set Ready
+    public void setReady()
+    {
+        //Update Ready Variable
+        readyToBegin = !readyToBegin;
+
+        if (player == null) player = LNetworkLobbyPlayer.getLocalLobbyPlayer();
+        if (player != null)
+        {
+            //Update CheckBox Sprite
+            if (readyToBegin) readyButtonCheckbox.sprite = readySprite;
+            else readyButtonCheckbox.sprite = unreadySprite;
+
+            //Update Lobby Player
+            player.readyToBegin = readyToBegin;
+        }
     }
 }
--- Assets/Scripts/Networking/LNetworkLobbyPlayer.cs
+++ Assets/Scripts/Networking/LNetworkLobbyPlayer.cs
@@ -6,47 +6,125 @@ using UnityEngine.Networking;
 
 public class LNetworkLobbyPlayer : NetworkLobbyPlayer
 {
-    //Variables
-    private Color playerClothColor;
-    private Color playerHairColor;
+    //Control Variables
+    public Player playerNum;
 
     //References
-    private Text playerTextRef;
-    private Material clothMaterialRef;
-    private Material hairMaterialRef;
+    private Material playerClothMaterialRef;
+    private Material playerHairMaterialRef;
+    private Material opponentClothMaterialRef;
+    private Material opponentHairMaterialRef;
 
-    // Use this for initialization
-    void Start ()
+    //On Start Authority
+    public override void OnStartAuthority()
     {
+        //Base Method
+        base.OnStartAuthority();
+
+        //Set Player
+        if (NetworkServer.active) playerNum = Player.Player1;
+        else playerNum = Player.Player2;
+
         //Set Initial References
-		if(isServer)
+        if (isServer)
         {
-            playerTextRef = LobbyPanelManager.Instance.player1Text;
-            playerTextRef.text = UserData.name;
-            clothMaterialRef = LobbyPanelManager.Instance.player1Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[1];
-            hairMaterialRef = LobbyPanelManager.Instance.player1Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[2];
-            playerClothColor = Color.blue;
-            playerHairColor = Color.green;
+            LobbyPanelManager.Instance.setPlayerButtons(playerNum);
+            playerClothMaterialRef = LobbyPanelManager.Instance.player1Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[1];
+            playerHairMaterialRef = LobbyPanelManager.Instance.player1Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[2];
+            opponentClothMaterialRef = LobbyPanelManager.Instance.player2Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[1];
+            opponentHairMaterialRef = LobbyPanelManager.Instance.player2Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[2];
         }
         else
         {
-            playerTextRef = LobbyPanelManager.Instance.player2Text;
-            playerTextRef.text = UserData.name;
-            clothMaterialRef = LobbyPanelManager.Instance.player2Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[1];
-            hairMaterialRef = LobbyPanelManager.Instance.player2Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[2];
-            playerClothColor = Color.red;
-            playerHairColor = Color.black;
+            LobbyPanelManager.Instance.setPlayerButtons(playerNum);
+            playerClothMaterialRef = LobbyPanelManager.Instance.player2Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[1];
+            playerHairMaterialRef = LobbyPanelManager.Instance.player2Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[2];
+            opponentClothMaterialRef = LobbyPanelManager.Instance.player1Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[1];
+            opponentHairMaterialRef = LobbyPanelManager.Instance.player1Lemming.GetComponentInChildren<SkinnedMeshRenderer>().materials[2];
         }
 
         //Finally...
-        changeColor(playerClothColor, playerHairColor);
+        CmdRequestNextHairColor();
+        CmdRequestNextClothColor();
+        CmdInformPlayerName(UserData.name, playerNum);
     }
-	
-    //Change Color & Update Material
-	public void changeColor(Color clothColor, Color hairColor)
+
+    //Get Local Lobby Player
+    public static LNetworkLobbyPlayer getLocalLobbyPlayer()
     {
-        playerTextRef.color = clothColor;
-        clothMaterialRef.color = clothColor;
-        hairMaterialRef.color = hairColor;
+        LNetworkLobbyPlayer[] list = FindObjectsOfType<LNetworkLobbyPlayer>();
+        for (int i = 0; i < list.Length; i++)
+        {
+            if (list[i].hasAuthority) return list[i];
+        }
+        return null;
+    }
+
+    //Get Opponent Lobby Player
+    public static LNetworkLobbyPlayer getOpponentLobbyPlayer()
+    {
+        LNetworkLobbyPlayer[] list = FindObjectsOfType<LNetworkLobbyPlayer>();
+        for (int i = 0; i < list.Length; i++)
+        {
+            if (!list[i].hasAuthority) return list[i];
+        }
+        return null;
+    }
+
+    //Inform Name
+    [Command]
+    public void CmdInformPlayerName(string playerName, Player playerNum)
+    {
+        if (playerNum == Player.Player1) LemmingCustomizer.Instance.player1Name = playerName;
+        else if (playerNum == Player.Player2) LemmingCustomizer.Instance.player2Name = playerName;
+    }
+
+    //Request Color Changes
+    [Command]
+    public void CmdRequestPreviousHairColor()
+    {
+        LemmingCustomizer.Instance.requestPreviousHairColor(playerNum);
+    }
+
+    [Command]
+    public void CmdRequestNextHairColor()
+    {
+        LemmingCustomizer.Instance.requestNextHairColor(playerNum);
+    }
+
+    [Command]
+    public void CmdRequestPreviousClothColor()
+    {
+        LemmingCustomizer.Instance.requestPreviousClothColor(playerNum);
+    }
+
+    [Command]
+    public void CmdRequestNextClothColor()
+    {
+        LemmingCustomizer.Instance.requestNextClothColor(playerNum);
+    }
+
+    //Update
+    private void FixedUpdate()
+    {
+        //Update Names
+        if (LemmingCustomizer.Instance.player1Name != "" && !LobbyPanelManager.Instance.player1Text.text.Equals(LemmingCustomizer.Instance.player1Name)) LobbyPanelManager.Instance.player1Text.text = LemmingCustomizer.Instance.player1Name;
+        if(LemmingCustomizer.Instance.player2Name != "" && !LobbyPanelManager.Instance.player2Text.text.Equals(LemmingCustomizer.Instance.player2Name)) LobbyPanelManager.Instance.player2Text.text = LemmingCustomizer.Instance.player2Name;
+
+        //Update Materials
+        if (playerNum == Player.Player1)
+        {
+            if(playerClothMaterialRef.color != LemmingCustomizer.Instance.player1ClothColor) playerClothMaterialRef.color = LemmingCustomizer.Instance.player1ClothColor;
+            if (playerHairMaterialRef.color != LemmingCustomizer.Instance.player1HairColor) playerHairMaterialRef.color = LemmingCustomizer.Instance.player1HairColor;
+            if (opponentClothMaterialRef.color != LemmingCustomizer.Instance.player2ClothColor) opponentClothMaterialRef.color = LemmingCustomizer.Instance.player2ClothColor;
+            if (opponentHairMaterialRef.color != LemmingCustomizer.Instance.player2HairColor) opponentHairMaterialRef.color = LemmingCustomizer.Instance.player2HairColor;
+        }
+        else if (playerNum == Player.Player2)
+        {
+            if (playerClothMaterialRef.color != LemmingCustomizer.Instance.player2ClothColor) playerClothMaterialRef.color = LemmingCustomizer.Instance.player2ClothColor;
+            if (playerHairMaterialRef.color != LemmingCustomizer.Instance.player2HairColor) playerHairMaterialRef.color = LemmingCustomizer.Instance.player2HairColor;
+            if (opponentClothMaterialRef.color != LemmingCustomizer.Instance.player1ClothColor) opponentClothMaterialRef.color = LemmingCustomizer.Instance.player1ClothColor;
+            if (opponentHairMaterialRef.color != LemmingCustomizer.Instance.player1HairColor) opponentHairMaterialRef.color = LemmingCustomizer.Instance.player1HairColor;
+        }
     }
 }
