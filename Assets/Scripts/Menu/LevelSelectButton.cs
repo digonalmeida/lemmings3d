bd9ef15a6014cf5c617f7883f3905f55f165a6d4
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-12-07 04:30:20 -0200

    Initial Progress on Level Selector

---
+++ Assets/Scripts/Menu/LevelSelectButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    //Variables
+    private Image imageReference;
+
+    //Start
+    private void Start()
+    {
+        imageReference = GetComponentInChildren<Image>();
+    }
+
+    //Update Image with level screenshot
+    public void updateImage(Sprite image)
+    {
+        if(image != null) imageReference.sprite = image;
+    }
+}
---
+++ Assets/Scripts/UI/LevelSelector/LoadLevels.cs
@@ -0,0 +1,24 @@
+﻿using LevelMap;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadLevels : MonoBehaviour
+{
+    //References
+    public GameObject levelSelectButtonPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+        List<MapAsset> mapAssets = MapManager.Instance.MapAssets;
+        for(int i = 0; i < mapAssets.Count; i++)
+        {
+            GameObject obj = Instantiate(levelSelectButtonPrefab, this.transform);
+            obj.GetComponent<Toggle>().group = this.GetComponent<ToggleGroup>();
+            LevelSelectButton button = obj.GetComponent<LevelSelectButton>();
+            button.updateImage(mapAssets[i].mapScrenshot);
+        }
+    }
+}
