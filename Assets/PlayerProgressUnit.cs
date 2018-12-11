66d1045bbe30410f844fc8fdc00cc2529040be4c
Thiago guardado Beltran <thiagoguardado@gmail.com>
2018-12-10 22:29:22 -0200

    ingame ui changes

---
+++ Assets/PlayerProgressUnit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProgressUnit : MonoBehaviour
+{
+
+    public GameObject background;
+    public GameObject foreground;
+    public ParticleSystem particles;
+    private ParticleSystem.EmissionModule emi;
+
+    public float minParticles;
+    public float maxParticles;
+
+    void Awake()
+    {
+        emi = particles.emission;
+    }
+
+    public void Reset()
+    {
+        background.SetActive(true);
+        foreground.SetActive(false);
+    }
+
+    public void TurnOn(float percIntensity)
+    {
+        foreground.SetActive(true);
+        emi.SetBurst(0, new ParticleSystem.Burst(0f, minParticles + percIntensity * (maxParticles - minParticles)));
+        particles.Play();
+    }
+}
